d24c2bff5fd33c83dab73fc8ec0070b4a41c6f88
Zain Ahmad <165908202+ZainSWE@users.noreply.github.com>
2025-05-07 18:02:19 -0400

    Update galleryScript.js

--- docs/galleryScript.js
+++ docs/galleryScript.js
@@ -6,7 +6,7 @@ fetch('dataset/gpuData.json')
       const div = document.createElement('div');
       div.className = 'gpu-card';
       div.innerHTML = `
-        <model-viewer src="../models/${gpu.Name}.glb" alt="${gpu.Name}" camera-controls disable-zoom></model-viewer>
+        <model-viewer src="models/${gpu.Name}.glb" alt="${gpu.Name}" camera-controls disable-zoom></model-viewer>
         <div class="gpu-info">
           <h3>${gpu.Name}</h3>
           <div class="gpu-details">
